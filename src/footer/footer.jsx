ab49f381f8fe4f4b148d93280005035d03b6537f
Josheirma <joshuaeirman@gmail.com>
2025-06-18 17:37:36 -0400

    Worked the header and footer.

---
+++ src/footer/footer.jsx
@@ -0,0 +1,12 @@
+import styles from './footer.module.css';
+function Footer() {
+  return (
+    <div className = {styles.footerContainer}>
+    <footer>
+      <p>&copy; 2025 My App. All rights reserved.</p>
+    </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/header/header.jsx
@@ -0,0 +1,16 @@
+import styles from './header.module.css';
+
+function Header() {
+  return (
+   
+    <div className = {styles.headerContainer} >
+    <header>
+       
+      My App
+   
+    </header>
+    </div> 
+  );
+}
+
+export default Header;
