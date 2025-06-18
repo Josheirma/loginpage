9246fdcd493d3e19a77182633f1ca29417bed218
Josheirma <joshuaeirman@gmail.com>
2025-06-18 17:59:42 -0400

    Message toggles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import Header from './header/header';
 import Footer from './footer/footer';
 import styles from './App.module.css';
+import { useState } from 'react';
+
+
+  
+
 function App() {
+
+  const [showMessage, setShowMessage] = useState(false);
+
+  const handleClick = () => {
+    setShowMessage(prev => !prev); // toggles between true and false
+  };
+
+
   return (
+    <div>
     <div className = {styles.container}>
     <div className= { styles.App}>
       <Header />
@@ -10,7 +24,26 @@ function App() {
         <p>Welcome to the main content!</p>
       </main>
       <Footer />
+    
+
+
     </div>
+
+
+    </div>
+
+<div className={styles.buttonContainer}>
+      
+      <button onClick={handleClick}>
+        {showMessage ? 'Hide' : 'Show'} Message
+      </button>
+      <div className = {styles.message}>
+      <div>
+      {showMessage && <p>This is the message!</p>}
+    </div>
+    </div>
+    </div>
+    
     </div>
   );
 }
